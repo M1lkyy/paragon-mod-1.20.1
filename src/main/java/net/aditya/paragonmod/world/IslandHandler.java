352e4a343823e7811e7ddb323c016ecfc8370b7c
adity <adityakxd@gmail.com>
2026-02-17 04:02:03 -0500

    finished citrine block, working on mysticity

---
+++ src/main/java/net/aditya/paragonmod/world/IslandHandler.java
@@ -0,0 +1,12 @@
+package net.aditya.paragonmod.world;
+
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class IslandHandler {
+    @SubscribeEvent
+    public void loadedDimension(EntityTravelToDimensionEvent event) {
+
+    }
+}
