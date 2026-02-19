903d9a45fdcd24cbfa4bbf474d8cf93553a559d1
adity <adityakxd@gmail.com>
2026-02-19 04:03:28 -0500

    slowly but surely working on mysticity...

--- src/main/java/net/aditya/paragonmod/world/IslandHandler.java
+++ src/main/java/net/aditya/paragonmod/world/IslandHandler.java
@@ -1,12 +1,40 @@
 package net.aditya.paragonmod.world;
 
-import net.minecraftforge.event.entity.EntityJoinLevelEvent;
-import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.*;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.StructureType;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.jetbrains.annotations.NotNull;
 
-public class IslandHandler {
+import java.util.Optional;
+
+@Mod.EventBusSubscriber(modid = "paragonmod")
+public class IslandHandler extends Structure {
+    public static final ResourceKey<Level> MYSTICITY = ResourceKey.create(
+            Registries.DIMENSION, ResourceLocation.fromNamespaceAndPath("paragonmod", "mysticity")
+    );
+
+    public IslandHandler(StructureSettings settings) {
+        super(settings);
+    }
+
     @SubscribeEvent
-    public void loadedDimension(EntityTravelToDimensionEvent event) {
+    public static void loadedDimension() {
+        // add code later
+    }
 
+    @Override
+    protected @NotNull Optional<GenerationStub> findGenerationPoint(GenerationContext generationContext) {
+        return Optional.empty();
+    }
+
+    @Override
+    public @NotNull StructureType<?> type() {
+        return null;
     }
 }
+
