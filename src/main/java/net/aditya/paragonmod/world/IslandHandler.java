d98089ee09b466b261afcf3352d6b68ef378d1e8
Aditya <adityakxd@gmail.com>
2026-02-21 00:00:25 -0500

    mysticity structure_set done

--- src/main/java/net/aditya/paragonmod/world/IslandHandler.java
+++ src/main/java/net/aditya/paragonmod/world/IslandHandler.java
@@ -1,11 +1,17 @@
 package net.aditya.paragonmod.world;
 
+import com.mojang.serialization.Codec;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.Structures;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.*;
 import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.StructurePiece;
+import net.minecraft.world.level.levelgen.structure.StructureStart;
 import net.minecraft.world.level.levelgen.structure.StructureType;
+import net.minecraft.world.level.levelgen.structure.structures.BuriedTreasureStructure;
+import net.minecraft.world.level.levelgen.structure.structures.RuinedPortalStructure;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.NotNull;
@@ -24,16 +30,18 @@ public class IslandHandler extends Structure {
 
     @SubscribeEvent
     public static void loadedDimension() {
-        // add code later
+        if (MYSTICITY != null) {
+            StructureType<BuriedTreasureStructure>
+        }
     }
 
     @Override
-    protected @NotNull Optional<GenerationStub> findGenerationPoint(GenerationContext generationContext) {
+    protected Optional<GenerationStub> findGenerationPoint(GenerationContext generationContext) {
         return Optional.empty();
     }
 
     @Override
-    public @NotNull StructureType<?> type() {
+    public StructureType<?> type() {
         return null;
     }
 }
